ae28773c2f9eea1e33507f662b19792b3be065b9
khalilcodes <khalilali.j@gmail.com>
2019-10-14 00:04:14 +0530

    updated

--- question57-2.js
+++ question57-2.js
@@ -1,41 +1,41 @@
 // Patterns - 2
 
-// var numbrs = "11111";
+var numbrs = "11111";
 
-// for (var i = 5; i > 0; i--) {
-//     if (i == 4) {
-//         numbrs = "2222";
-//     } else if (i == 3) {
-//         numbrs = "333";
-//     } else if (i == 2) {
-//         numbrs = "22";
-//     } else if (i == 1) {
-//         numbrs = "1";
-//     }
-//     console.log(numbrs);
-// }
+for (var i = 5; i > 0; i--) {
+    if (i == 4) {
+        numbrs = "2222";
+    } else if (i == 3) {
+        numbrs = "333";
+    } else if (i == 2) {
+        numbrs = "22";
+    } else if (i == 1) {
+        numbrs = "1";
+    }
+    console.log(numbrs);
+}
 
-// for (var j = 1; j <= 5; j++) {
-//     var num = "54321";
-//     num = num.replace(j,"*");
-//     console.log(num);
-// }
+for (var j = 1; j <= 5; j++) {
+    var num = "54321";
+    num = num.replace(j,"*");
+    console.log(num);
+}
 
-// str = "";
+str = "";
 
-// for (var i = 0; i < 5; i++) {
-//     str = str + "*";
-//     console.log(str);
-// }
+for (var i = 0; i < 5; i++) {
+    str = str + "*";
+    console.log(str);
+}
 
-// console.log("");
+console.log("");
 
-// str2 = "******";
+str2 = "******";
 
-// for (var j = 5; j>0; j--) {
-//     str2 = str2.slice(1) ;
-//     console.log(str2);
-// }
+for (var j = 5; j>0; j--) {
+    str2 = str2.slice(1) ;
+    console.log(str2);
+}
 
 // Write a program to print Floyd’s triangle as shown below for given N lines:
 
